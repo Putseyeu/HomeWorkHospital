7c790aea14a9b02136e37fe2b6841fd93f4f1d9f
Putseyeu <putseyeu.a.d@gmail.com>
2021-10-11 19:55:32 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int totalPeople;
+            Console.Write("Введите кол-во старушек:");
+            totalPeople = Convert.ToInt32(Console.ReadLine());
+
+            int timeOnePatient = 10;
+            int minutesPerHour = 60;
+
+            int timeAllWaiting = totalPeople * timeOnePatient;
+            int hourWaiting = timeAllWaiting / minutesPerHour;
+            int minutesWaiting = timeAllWaiting % minutesPerHour;
+
+            Console.Write($"Вы должны отстоять в очереди {hourWaiting} часа и {minutesWaiting} минут.");
+        }
+    }
+}
